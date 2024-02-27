730084f74cba7cc9f16267aadacd0941b67fedff
RushaliChaudhari <121595155+RushaliChaudhari@users.noreply.github.com>
2024-02-27 21:46:53 +0530

    Add files via upload

---
+++ Pattern/Pattern_six.java
@@ -0,0 +1,27 @@
+package Pattern;
+
+public class Pattern_six {
+   public static void main(String[] args) {
+        int n=10;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(i+j==n-1 ||i==j)
+                {
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+                 
+                
+            }
+            System.out.println();
+        }
+      
+    }
+}
+
+  
+
